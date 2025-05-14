6548ee41bd22110abd187418db70dc69ff58e5b8
Anil-S-2003 <anianni9880@gmail.com>
2025-05-14 18:03:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def send_message():
 if __name__ == '__main__':
     os.makedirs('uploads', exist_ok=True)
     port = int(os.environ.get("PORT",5000))
-    app.run(host = '0.0.0.0',port=10000)
+    app.run(host = '0.0.0.0', port=port)
 
